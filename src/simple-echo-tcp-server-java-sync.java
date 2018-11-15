e240deb47c2496f5e409591e7e5f06f4b6f4103d
elaygl <elaygl@gmail.com>
2018-11-15 09:40:40 +0200

    added c#, java and python servers

---
+++ src/simple-echo-tcp-server-java-sync.java
@@ -0,0 +1,59 @@
+package com.echoserverexample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.nio.channels.Channels;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        int port = 2424;
+        ServerSocketChannel server = null;
+        try {
+            server = ServerSocketChannel.open();
+            server.bind(new InetSocketAddress(port));
+            while (true){
+                final SocketChannel socket = server.accept();
+                // init new client;
+                executor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        BufferedReader reader = null;
+                        PrintWriter writer = null;
+                        try {
+                            reader = new BufferedReader(Channels.newReader(socket, "utf-8"));
+                            writer = new PrintWriter(Channels.newWriter(socket, "utf-8"));
+                            while(socket.isConnected()){
+                                String command = reader.readLine();
+                                //do some processing
+                                writer.println(command);
+                                writer.flush();
+                            }
+                        }
+                        catch (IOException ex){
+                            if (!socket.isConnected()){
+                                ex.printStackTrace();
+                            }
+                        }
+                        finally {
+                            if (reader != null) try {reader.close();} catch (Exception ex){}
+                            if (writer != null) try {writer.close();} catch (Exception ex){}
+                        }
+                    }
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (server != null) try {server.close();} catch (Exception ex){}
+        }
+    }
+}
---
+++ src/simple-echo-tcp-server-net-sync.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace SomeServerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var port = 2424;
+            var listener = TcpListener.Create(port);
+            listener.Start();
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                // handle new client connection
+                Task.Run(() =>
+                {
+                    using (var stream = client.GetStream())
+                    using (var reader = new StreamReader(stream))
+                    using (var writer = new StreamWriter(stream) {AutoFlush = true})
+                        while (client.Connected)
+                        {
+                            try
+                            {
+                                var line = reader.ReadLine();
+                                // process command
+                                writer.WriteLine(line);
+                            }
+                            catch (IOException)
+                            {
+                                if (client.Connected) throw;
+                            }
+                        }
+                });
+            }
+        }
+    }
+}
