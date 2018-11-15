7c39717d69f8f9fb58bd07deaa0ce6ca42eac475
elaygl <elaygl@gmail.com>
2018-11-15 10:50:58 +0200

    changed c# server to a class

--- src/simple-echo-tcp-server-net-sync.cs
+++ src/simple-echo-tcp-server-net-sync.cs
@@ -4,9 +4,9 @@ using System.Threading.Tasks;
 
 namespace SomeServerApplication
 {
-    class Program
+    class TcpServer
     {
-        static void Main(string[] args)
+        public void Start()
         {
             var port = 2424;
             var listener = TcpListener.Create(port);
@@ -19,7 +19,7 @@ namespace SomeServerApplication
                 {
                     using (var stream = client.GetStream())
                     using (var reader = new StreamReader(stream))
-                    using (var writer = new StreamWriter(stream) {AutoFlush = true})
+                    using (var writer = new StreamWriter(stream) { AutoFlush = true })
                         while (client.Connected)
                         {
                             try
